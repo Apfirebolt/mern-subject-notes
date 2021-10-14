1daf08b707bae35de4273f911244e14389d18134
Amit P <aspper20@gmail.com>
2021-10-14 06:46:43 +0530

    feat: auth backend routes added

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
 import cors from 'cors'
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/error-handler.js'
+
+// import routes
+import authRoutes from './routes/auth-routes.js'
 
 dotenv.config()
 
@@ -17,11 +23,50 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json())
 app.use(cors())
 
+app.use('/api/auth', authRoutes)
+
 app.get('/', (req, res) => {
     res.json({ message: 'Success' })
 })
 
+app.use(notFound)
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5000
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "API for Subject Notes App in Express and React",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Apfirebolt",
+        url: "http://apgiiit.com",
+        email: "aspper@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:5000/api/auth",
+      },
+    ],
+  },
+  apis: ["./routes/auth-routes.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+);
 
 app.listen(
   PORT,
